51945996c694929ce302a46bfad1e0c275fdf591
Francois Donze <francois.donze@hpe.com>
2025-02-19 18:08:42 +0100

    Ran migration script
    Signed-off-by: Francois Donze <francois.donze@hpe.com>

---
+++ index.page.tsx
@@ -0,0 +1,43 @@
+import * as React from 'react';
+
+// import { LandingLayout } from "@redocly/ui";
+// import { FlexSection, Flex, navigate } from "@redocly/ui";
+// 
+// import { NavBar } from "@redocly/ui";
+// import {
+//   Grid,
+//   Page,
+//   PageContent,
+//   ResponsiveContext,
+//   Grommet,
+//   Section,
+//   Box,
+//   Card,
+//   CardBody,
+//   CardHeader,
+//   Heading,
+//   Text,
+//   Button,
+//   Paragraph,
+//   Anchor
+// } from "grommet";
+// import { hpe } from "grommet-theme-hpe";
+// 
+// import { Directions, Services, Group, Tools } from "grommet-icons";
+// 
+// import bannerBg from "./images/banner.jpg";
+
+
+
+export const frontmatter = {
+  "seo": {
+    "title": "HPE server management with Redfish®"
+  }
+};
+
+export default function Page() {
+  return <div>TODO: migrate manually</div>;
+  /* Original code:
+    <Grommet theme={hpe} />
+  */
+}
