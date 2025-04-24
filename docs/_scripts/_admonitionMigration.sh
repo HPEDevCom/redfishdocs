27e265872e52639107e3c702e5b6d41bce05a663
Francois Donze <francois.donze@hpe.com>
2025-04-24 12:37:59 +0200

    Should be final commit
    Signed-off-by: Francois Donze <francois.donze@hpe.com>

--- docs/_scripts/_admonitionMigration.sh
+++ docs/_scripts/_admonitionMigration.sh
@@ -7,11 +7,22 @@
 # in https://github.com/redocly-demo/migrate-portal/blob/main/bin.ts#L795
 # from Redocly.
 #
-# Version 0.1
+# Version 0.2
 
  
+rootDir="/Git-Repo/ProtoRedfishDocs"
+cd $rootDir/docs/_scripts
 
-sed -E -i \
-  's/:::([^ ]+) (.+)/{% admonition type="\1" name="\2" %}/g ; s?::: *$?{% /admonition %}?g' $1
+mdFileList=$(find $rootDir -type f -name "*.md" -not -path "*/node_modules/*" -not -path "*/.git/*" -not -path "*/README.md")
+#mdFileList="$rootDir/docs/redfishServices/ilos/supplementDocuments/securityService.md"
 
-
+for file in $mdFileList
+do
+  echo Processing $file
+  dos2unix $file &> /dev/null
+  sed -i -E  \
+    's/::::/:::/g' $file   
+  sed -i -E  \
+    's/:::([^ ]+) ([^ ]+)/{% admonition type="\1" name="\2" %}/g ; s?::: *$?{% /admonition %}?g' $file
+done
+echo
