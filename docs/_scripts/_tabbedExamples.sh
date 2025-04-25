1def8772321791dd258d2a72ad1320eac9c5608e
Francois Donze <francois.donze@hpe.com>
2025-04-25 19:07:35 +0200

    Progressing...
    Signed-off-by: Francois Donze <francois.donze@hpe.com>

--- docs/_scripts/_tabbedExamples.sh
+++ docs/_scripts/_tabbedExamples.sh
@@ -3,22 +3,29 @@
 # This script updates Redocly/Workflows tabbed examples
 # into Reunite/Realm tabbed examples.
 #
-#
 # Version 0.1
 
  
 rootDir="/Git-Repo/ProtoRedfishDocs"
 cd $rootDir/docs/_scripts
 
-mdFileList=$(find $rootDir -type f -name "*.md" -not -path "*/node_modules/*" -not -path "*/.git/*" -not -path "*/README.md -not -path */.github/*")
-
-# Check if processing is really needed
+#mdFileList=$(find $rootDir -type f -name "*.md" -not -path "*/node_modules/*" -not -path "*/.git/*" -not -path "*/README.md -not -path */.github/*")
+mdFileList="$rootDir/docs/redfishservices/ilos/supplementdocuments/securityService.md" 
 
 for file in $mdFileList
 do
   echo Processing $file
   dos2unix $file &> /dev/null
-  
+
+  # Wrap all examples with markdoc tab tag
+  sed -i -E 's/```([[:alpha:]]+) ([[:alnum:] ]+)/{% tab label="\2" %}\n```\1 \2/'  $file
+  sed -i -E '/```$/a\
+  {% \/tab %}
+  ' $file
+
+  # Replace the placeholder with the original code block
+  #sed -i 's/```label_placeholder/```/' $file
+
   echo -e "Done \n"
 done
 echo
